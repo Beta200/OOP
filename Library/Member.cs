66bfec8044278f7832948181b6d20ad89f297ccd
Tamás Bezák <84201807+Beta200@users.noreply.github.com>
2023-11-28 17:18:19 +0100

    Add files via upload

---
+++ Library/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class Member
+    {
+        public string name { get; set; }
+        public Dictionary<Book,long> borrowed { get; set; }
+
+        public Member(string name)
+        {
+            this.borrowed = new Dictionary<Book, long>();
+            this.name = name;
+        }
+    }
+}
---
+++ Library/NatScience.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class NatScience : Book
+    {
+        public NatScience(string title, string author, string publisher, string isbn, int pages, int id, Rarity rarity) : base(title, author, publisher, isbn, pages, id, rarity) { }
+
+        public override bool isNatScience() { return true; }
+    }
+}
